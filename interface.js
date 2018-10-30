9609e0ab042c2ee1299cecf30fc0f693aa276c83
Muna <mmhussein125@gmail.com>
2018-10-30 16:03:47 +0000

    can change temperature in interface

--- interface.js
+++ interface.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature());
+
+  $('#up').click(function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature());
+  })
+
+  $('#down').click(function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature());
+  })
 })

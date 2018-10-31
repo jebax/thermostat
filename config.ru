1e8329a2b15a33777404258f3329599275084258
jebax <joel.eliot.baxter@gmail.com>
2018-10-31 15:32:01 +0000

    added gemfile and rackup config.ru

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './sinatra_api.rb'
+
+run ThermostatAPI
--- sinatra_api.rb
+++ sinatra_api.rb
@@ -1,6 +1,7 @@
-require 'sinatra'
+require 'sinatra/base'
 
 class ThermostatAPI < Sinatra::Base
-  
 
+
+  run! if app_file == $PROGRAM_NAME
 end
